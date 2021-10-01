e25a8249da5c683bad47be78b0ba42137d39756e
Jotap-ux <juanpablochato123@gmail.com>
2021-10-01 12:32:45 -0300

    confirmo2

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthenticationService } from 'src/app/services/authentication.service';
-
+//importamos clase Usuario creada en 'model'
+import{ Usuario } from '../model/usuario.interfaces';
+//para poder enviar mensajes(en este caso de que el usuario o pass esta incorrecta)
+import { ToastController } from '@ionic/angular';
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
@@ -8,13 +11,32 @@ import { AuthenticationService } from 'src/app/services/authentication.service';
 })
 export class LoginPage implements OnInit {
 
-  constructor(private authSer: AuthenticationService) { }
+  constructor(
+    private authServ: AuthenticationService,
+    private toastCtrl: ToastController
+    ) { }
 
+  usuario : Usuario={
+    nombre:'',
+    password:''
+  }
   ngOnInit() {
   }
 
   loginUser(){
-    this.authSer.login();
+    console.log(this.usuario);
+    if(this.usuario.nombre=="tito" && this.usuario.password=="123"){
+      this.authServ.login();
+    }else{
+      this.toastCtrl.create({
+        message:"Usuario o pass incorrecta",
+        duration: 3000,
+        position:"middle"
+      }).then((toast)=>{
+        toast.present();
+      })
+    }
+    
   }
 
 }
